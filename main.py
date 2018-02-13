f09175a79dcd4a0039b4c9619aca2ce83b8f0446
Joey Mandarino <joey@leotta.com>
2018-02-13 15:40:02 -0500

    added slope calculation and automatic axvlines

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+# python 3.3 or newer
 from datetime import datetime
+from datetime import timedelta
 import time
 from datetime import timezone
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 
-years = mdates.YearLocator()   # every year
-months = mdates.MonthLocator()  # every month
-yearsFmt = mdates.DateFormatter('%Y')
+# TODO: python-dateutil for parsing and handling timezones
+# TODO: fix timezone issues
+# TODO: meter reading automation (open CV or ML)
+# TODO: automate the creation of axvlines
 
 # stores times
 t = [
@@ -79,6 +82,45 @@ axvlines_raw = ["2/7/18 0:01", "2/8/18 0:01", "2/9/18 0:01", "2/10/18 0:01", "2/
 # stores processed lines (times converted to epoch time)
 axvlines = []
 
+"""
+HELPER FUNCTIONS
+"""
+def _calc_slope(x,y,x1,y1):
+    return (x1 - x)/(y1 - y)
+
+
+def _calc_y_incpt(slope, x, y):
+    return y - (slope * x)
+
+
+def _calc_y_value(slope, x, b):
+    return slope * x + b
+
+
+"""
+FUNCTIONS
+"""
+# TODO: fix the format to be more readable
+
+
+def calc_est_y(x_poi, x,y, x1, y1):
+    """
+    Calculate an estimate value for y for our x value Point of Interest
+
+    1. find equation for line, y = mx +b
+    2. plug in x to find y
+
+    :param x_poi: x value we want to find y for
+    :param x: x value before x_poi
+    :param y: y value before x_poi
+    :param x1: x value after x_poi
+    :param y1: y value after _poi
+    :return: Estimate of y value at that time
+    """
+    m = _calc_slope(x,y,x1,y1)
+    b = _calc_y_incpt(m,x,y)
+    return _calc_y_value(m, x_poi, b)
+
 
 def convert(str):
     """
@@ -90,14 +132,46 @@ def convert(str):
     # 2/5/18 00:00"
     d = datetime.strptime(str, "%m/%d/%y %H:%M")
     unixtime = time.mktime(d.timetuple())
-    return d
+    return unixtime
+
+
+# TODO: in-> epoch out->epoch or datetime->datime
+def init_axvlines(min, max):
+    """
+    Creates an array of date markers in epoch time  from a start and end date
+    :param min: start date (in epoch time)
+    :param max: end date (in epoch time)
+    :return:
+    """
+    if min == None or min == max:
+        return []
+
+    output = []
+    # get the midnight value (12:01/0:01) for the start time
+    # indicate the the start of the first day
+    cur = datetime.fromtimestamp(min)
+    cur = cur.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+    output.append(cur.timestamp())
+    #TODO: only work on datetime objects and
+    while(cur.timestamp() < max):
+        cur = cur + timedelta(days=1)
+        output.append(cur.timestamp())
+
+    return output
 
 
 for x in t:
-    dates.append(convert(x))
+    date = datetime.strptime(x, "%m/%d/%y %H:%M")
+    dates.append(date)
 
+start = convert(t[0])
+end = convert(t[-1])
+
+axvlines_raw = init_axvlines(start, end)
 for x in axvlines_raw:
-    axvlines.append(convert(x))
+    axvlines.append(datetime.fromtimestamp(x))
 
 print(dates)
 
