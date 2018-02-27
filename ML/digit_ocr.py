f863e09b248749c8402a03d36021ad347b6e6dc7
Joey Mandarino <joey@leotta.com>
2018-02-27 15:45:14 -0500

    formatting and trimming

--- ML/digit_ocr.py
+++ ML/digit_ocr.py
@@ -1,13 +1,12 @@
+import os
 import numpy as np
 import cv2
 import imutils
-import PIL
 from imutils.perspective import four_point_transform
-import os
 from imutils import contours
 
 def process_image(path_to_img):
-    # creates an edge map
+    # creates an edge map and convert to gray scale
     image = cv2.imread(path_to_img)
     image = imutils.resize(image, height=500)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -33,6 +32,7 @@ def process_image(path_to_img):
         if len(approx) == 4:
             displayCnt = approx
             break
+    # find the display aka the first 4 sided object we see
     warped = four_point_transform(gray, displayCnt.reshape(4, 2))
     output = four_point_transform(image, displayCnt.reshape(4, 2))
 
@@ -40,52 +40,32 @@ def process_image(path_to_img):
     return warped, output
 
 
-def trim(im):
-    bg = PIL.Image.new(im.mode, im.size, im.getpixel((0,0)))
-    diff = PIL.ImageChops.difference(im, bg)
-    diff = PIL.ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
-
 
 if __name__ == '__main__':
+    """Process the image twice, the first time we find the bounding area of the contours of
+    the numbers
+    
+    the next time processing we extract the numbers and process
+    
+    """
 
+    # process image and obtain display
     display, output = process_image('img/test.jpg')
-
-    # cv2.imshow("output without drawcontours()", output)
-    # cv2.waitKey(0)
 
     thresh = cv2.threshold(display, 0, 255,
                                cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-    # cv2.imshow("output without drawcontours()", thresh)
-    # cv2.waitKey(0)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-    # thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-
-
-    # cv2.imshow("output without drawcontours()", thresh)
-    # cv2.waitKey(0)
-
     thresh_copy = thresh.copy()
     thresh_copy = cv2.dilate(thresh_copy, kernel, iterations=2)
     # thresh_copy = cv2.erode(thresh_copy, kernel, iterations=2)
     _, cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    #_, cnts, hierarchy = cv2.findContours(thresh_copy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     digitCnts = []
     for c in cnts:
         (x, y, w, h) = cv2.boundingRect(c)
         if w > 4 and h > 12:
             digitCnts.append(c)
-
-    cnt = digitCnts[0]
-
-    try: hierarchy = hierarchy[0]
-    except: hierarchy = []
 
     height, width, _ = output.shape
     min_x, min_y = width, height
@@ -99,48 +79,31 @@ if __name__ == '__main__':
         # if w > 80 and h > 80:
         #     cv2.rectangle(output, (x,y), (x+w,y+h), (255, 0, 0), 2)
 
+
+    # crop the threshold copy and the B&W output based on where the numbers are
     thresh_crop = thresh[min_y:max_y, min_x:max_x]
     out_crop = output[min_y:max_y , min_x:max_x]
-    # cv2.imshow("output without drawcontours()", out_crop)
-    # cv2.waitKey(0)
 
-
-
-    kernel = np.ones((1,1),np.uint8)
-    # thresh_crop = cv2.erode(thresh_crop, kernel, iterations=2)
-    # thresh_crop = cv2.dilate(thresh_crop, kernel, iterations=2)
-    # thresh_crop = cv2.morphologyEx(thresh_crop, cv2.MORPH_CLOSE, kernel)
-
-
+    # processing the image 2 times ended up being more successful
     # refind contours after croping image
     gray = cv2.cvtColor(out_crop, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255,
                            cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
     # remove noise
-    kernel = np.ones((1,1),np.uint8)
+    kernel = np.ones((1, 1), np.uint8)
     thresh = cv2.erode(thresh, kernel, iterations=2)
-    kernel = np.ones((1,1),np.uint8)
+    kernel = np.ones((1, 1), np.uint8)
     thresh = cv2.dilate(thresh, kernel, iterations=5)
 
-    # make them longer
-    kernel = np.ones((3,1),np.uint8)
+    # then stretch the image vertically to connect the segments
+    kernel = np.ones((3, 1), np.uint8)
     thresh = cv2.dilate(thresh, kernel, iterations=1)
 
-
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-    # thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
-    # cv2.drawContours(thresh, digitCnts, -1, (0,255,0), 1)
-    # cv2.imshow("output without drawcontours()", thresh)
-    # cv2.waitKey(0)
-
-    # thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
     _, cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-
+    # processing again and resetting variables
     digitCnts = []
     boxes = []
     for c in cnts:
@@ -148,23 +111,20 @@ if __name__ == '__main__':
         if w > 4 and h > 12:
             digitCnts.append(c)
 
-    # process the image 2 times, first time gets a close contour
-    # TODO: get path to this file
-    os.environ["TESSDATA_PREFIX"] = "/Users/joey/PycharmProjects/meter-reader/ML/tessdata"
-
     # process individual characters
     output_list = []
     digitCnts = contours.sort_contours(digitCnts,
-                                 method="left-to-right")[0]
-    BLACK = [0,0,0]
+                                 method="left-to-right")[0] # this might not be working
+
+    BLACK = [0, 0, 0] # define color for border
     for counter, c in enumerate(digitCnts[::-1]):
         # extract the digit ROI
         (x, y, w, h) = cv2.boundingRect(c)
+        # cut image down to size of contour's max boundaries
         roi = thresh[y:y + h, x:x + w]
-        constant = cv2.copyMakeBorder(roi,5,5,10,10,cv2.BORDER_CONSTANT,value=BLACK)
+        # add a border, this has helped with image processing via KNN/SVM
+        constant = cv2.copyMakeBorder(roi,5,5,10,10,cv2.BORDER_CONSTANT, value=BLACK)
         cv2.imwrite("test-"+str(counter)+".bmp", constant)
-
-
 
     exit(0)
 
